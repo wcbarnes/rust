61b7e4765af0773d85e8601c59d10cfda99db414
William Barnes <barnesw91291@gmail.com>
2017-02-04 12:11:37 -0700

    print sum function

--- projects/basic_functions/src/main.rs
+++ projects/basic_functions/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
     print_number(5);
+    print_sum(10, 15);
 }
 
 fn print_number(x: i32) {
     println!("x is: {}", x);
 }
 
+fn print_sum(x: i32, y: i32) {
+    println!("Sum is {}", x + y);
+}
