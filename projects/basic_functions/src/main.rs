d7edd099d78afea078fe11f55c92131a4197f16c
William Barnes <barnesw91291@gmail.com>
2017-02-04 12:38:58 -0700

    wrote fizzbuzz :fire:

--- projects/basic_functions/src/main.rs
+++ projects/basic_functions/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     print_number(5);
     print_sum(10, 15);
     print_number(add_one(10));
+    fizzbuzz(15);
 }
 
 fn print_number(x: i32) {
@@ -14,4 +15,18 @@ fn print_sum(x: i32, y: i32) {
 
 fn add_one(x: i32) -> i32 {
     x + 1
+}
+
+fn fizzbuzz(x: i32) {
+    for y in 1..x+1 {
+        if y % 15 == 0 {
+            println!("{}. FIZZBUZZ", y);
+        } else if y % 5 == 0 {
+            println!("{}. BUZZ", y);
+        } else if y % 3 == 0 {
+            println!("{}. FIZZ", y);
+        } else {
+            println!("{}", y);
+        }
+    }
 }
