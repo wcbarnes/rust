16ce73b41274f857dc172405951d5767cc3e3dfb
William Barnes <barnesw91291@gmail.com>
2017-02-04 12:14:22 -0700

    added a function with an implicit return

--- projects/basic_functions/src/main.rs
+++ projects/basic_functions/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     print_number(5);
     print_sum(10, 15);
+    print_number(add_one(10));
 }
 
 fn print_number(x: i32) {
@@ -9,4 +10,8 @@ fn print_number(x: i32) {
 
 fn print_sum(x: i32, y: i32) {
     println!("Sum is {}", x + y);
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
 }
